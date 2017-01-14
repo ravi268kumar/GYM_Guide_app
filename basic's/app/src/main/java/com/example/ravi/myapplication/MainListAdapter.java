88d3386c41eafadc1feba649af14ffecc137913d
ravi268kumar <201452056@iiitvadodara.ac.in>
2017-01-14 23:59:23 +0530

    adding sub exr

--- basic's/app/src/main/java/com/example/ravi/myapplication/MainListAdapter.java
+++ basic's/app/src/main/java/com/example/ravi/myapplication/MainListAdapter.java
@@ -3,6 +3,7 @@ package com.example.ravi.myapplication;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +25,7 @@ public class MainListAdapter extends ArrayAdapter<Exercise> {
     Context context;
     ImageView imageView;
     TextView textView;
+    CardView list_cardView;
 
 
     public MainListAdapter(Context context , ArrayList<Exercise> exercises) {
@@ -42,20 +44,19 @@ public class MainListAdapter extends ArrayAdapter<Exercise> {
         View view = LayoutInflater.from(context).inflate(R.layout.sample_item,null,false);
         imageView = (ImageView) view.findViewById(R.id.imageView);
         textView = (TextView) view.findViewById(R.id.item_title);
-
+        list_cardView =(CardView) view.findViewById(R.id.card_view_id);
         imageView.setImageResource(exercises.get(position).img_id);
         textView.setText(exercises.get(position).title);
-
-        view.setOnClickListener(new View.OnClickListener() {
+        list_cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(context,SubExerciseActivity.class);
                 intent.putExtra("title",exercises.get(position).title);
                 context.startActivity(intent);
-
             }
         });
+
+
 
         return view;//super.getView(position, convertView, parent);
     }
--- basic's/app/src/main/java/com/example/ravi/myapplication/SubExerciseActivity.java
+++ basic's/app/src/main/java/com/example/ravi/myapplication/SubExerciseActivity.java
@@ -21,8 +21,32 @@ public class SubExerciseActivity extends AppCompatActivity {
         String title = getIntent().getExtras().getString("title");
 
         first_sub = new ArrayList<>();
-        first_sub.add(new SubExercise(R.drawable.img, "Abroller"));
-        first_sub.add(new SubExercise(R.drawable.img2, "Biceps"));
+        first_sub.add(new SubExercise(R.drawable.ab_roller, "AB ROLLER"));
+        first_sub.add(new SubExercise(R.drawable.bicycle_crunches, "BICYCLE CRUNCHES"));
+
+        first_sub.add(new SubExercise(R.drawable.bottoms_up, "BOTTOMS UP STANDING"));
+        first_sub.add(new SubExercise(R.drawable.abs_crunch, "CRUNCHES"));
+
+        first_sub.add(new SubExercise(R.drawable.decline_crunches, "DECLINE CRUNCHES"));
+        first_sub.add(new SubExercise(R.drawable.hanging_upside, "HANGING UPSIDE"));
+
+        first_sub.add(new SubExercise(R.drawable.cable_seated_crunches, "CABLESEATED CRUNCHES"));
+        first_sub.add(new SubExercise(R.drawable.lying_leg_raise, "LYING LEG RAISE"));
+
+        first_sub.add(new SubExercise(R.drawable.plank, "PLANK"));
+        first_sub.add(new SubExercise(R.drawable.side_lateral_bend, "SIDELATERAL BENT"));
+
+        first_sub.add(new SubExercise(R.drawable.side_plank, "SIDE PLANK"));
+        first_sub.add(new SubExercise(R.drawable.v_up_abs, "V-UP ABS"));
+
+        first_sub.add(new SubExercise(R.drawable.bent_knee_hip_raise, "NENT KNEE HIP RAISE"));
+        first_sub.add(new SubExercise(R.drawable.butts_up, "BUTTS UP"));
+
+        first_sub.add(new SubExercise(R.drawable.cable_twists, "CABLE TWISTS"));
+        first_sub.add(new SubExercise(R.drawable.decline_oblique_crun, "DECLINE OBLIQUE CRUNCHES"));
+
+
+
 
 
         second_sub = new ArrayList<>();
